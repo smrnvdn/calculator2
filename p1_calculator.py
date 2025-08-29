dfda5264f23c65ee54e047674b8ab5a2d35cf289
smrnvdn <99199354+smrnvdn@users.noreply.github.com>
2025-08-29 13:09:20 +0300

    fix: correct operation input type from int to str (#22)
    closes #22

--- p1_calculator.py
+++ p1_calculator.py
@@ -1,20 +1,37 @@
-def calculator():
+"""This is a simple calculator that can calculate the sum, difference, product
+and division of two numbers it takes two numbers and an operation as input and
+prints the result of the operation it also checks if the operation is valid and
+if the second number is not zero for division it also checks if the operation
+is valid and if the second number is not zero for division."""
+
+
+def calculator() -> None:
+    """
+    this function is used to calculate the sum, difference, product and division of two numbers
+    input:
+        no_1: int
+        operation: str
+        no_2: int
+    output:
+        print the result of the operation
+    """
     no_1 = int(input("enter your first number"))
-    operation = int(input("enter  arithmetic operation  like +,-,*,/"))
+    operation = input("enter  arithmetic operation  like +,-,*,/")
     no_2 = int(input("enter your  second number"))
 
-    if operation =='+':
+    if operation == "+":
         print(f"the sum of {no_1} + {no_2} are {no_1 + no_2}")
-    elif  operation =='-':
+    elif operation == "-":
         print(f"the diff of {no_1}  - {no_2} are {no_1 - no_2}")
-    elif operation =='*':
+    elif operation == "*":
         print(f"the product of {no_1} * {no_2} are {no_1 * no_2}")
-    elif  operation =='/':
+    elif operation == "/":
         if no_2 == 0:
             print("it gives infinity as it is not divided by zero")
         else:
             print(f"the sum of {no_1}  / {no_2} are {no_1 / no_2}")
     else:
         print("invalid output")
+
 
 calculator()
